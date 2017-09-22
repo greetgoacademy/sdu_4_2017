2778fbd33481912478f442de14381e45e5ec67c9
stursyn <stursyn@greet-go.com>
2017-09-22 17:01:54 +0600

    full functional stand of education

---
+++ education.parent/education.controller/src/main/java/kz/greetgo/education/controller/register/IntroductionRegister.java
@@ -0,0 +1,9 @@
+package kz.greetgo.education.controller.register;
+
+import kz.greetgo.education.controller.model.IntroductionInfo;
+
+public interface IntroductionRegister {
+
+  IntroductionInfo getIntroductionInfo();
+
+}
