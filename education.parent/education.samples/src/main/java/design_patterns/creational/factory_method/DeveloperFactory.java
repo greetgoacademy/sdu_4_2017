6553689c842981dc93c69966f719a143e29f0b28
itukibayev <itukibayev@greet-go.com>
2017-10-06 16:06:34 +0600

    Added new module education.sample. Added examples with AbstractFactory/MethodFactory patterns.

---
+++ education.parent/education.samples/src/main/java/design_patterns/creational/factory_method/DeveloperFactory.java
@@ -0,0 +1,15 @@
+package design_patterns.creational.factory_method;
+
+public class DeveloperFactory {
+
+  public static Developer getDeveloperByProgrammingLanguage(ProgrammingLanguageType programmingLanguageType) {
+    switch (programmingLanguageType) {
+      case JAVA:
+        return new JavaDeveloper();
+      case CPP:
+        return new CppDeveloper();
+      default:
+        throw new IllegalArgumentException("No developer for " + programmingLanguageType);
+    }
+  }
+}
---
+++ education.parent/education.samples/src/main/java/design_patterns/creational/factory_method/ProgrammingLanguageType.java
@@ -0,0 +1,5 @@
+package design_patterns.creational.factory_method;
+
+public enum ProgrammingLanguageType {
+  JAVA, CPP, PYTHON, PHP;
+}
