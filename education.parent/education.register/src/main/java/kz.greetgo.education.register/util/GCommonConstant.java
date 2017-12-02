c6c34028bac776ea94a31751146a4cafc8f3434a
stursyn <stursyn@greet-go.com>
2017-12-02 14:01:42 +0600

    change common constants

--- education.parent/education.register/src/main/java/kz.greetgo.education.register/util/GCommonConstant.java
+++ education.parent/education.register/src/main/java/kz.greetgo.education.register/util/GCommonConstant.java
@@ -4,6 +4,8 @@ package kz.greetgo.education.register.util;
  * Created by asset on 10/27/17.
  */
 public class GCommonConstant {
-  public static String username="stursyn@greet-go.com";
-  public static String password="Sultanbek1205";
+  public static String username="***********";
+  public static String password="***********";
+
+  public static String GC_AUTH="GCAuth";
 }
